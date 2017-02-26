7a7c45633868fc062ddc86a13d65b6947cf2971f
becolomochi <becolomochi@gmail.com>
2017-02-26 18:09:37 +0900

    リンクにスライド追加

--- application/views/gezicht/partial/slide-top.php
+++ application/views/gezicht/partial/slide-top.php
@@ -13,5 +13,53 @@ $(function(){
 			{ src: dir + "tamayodo_1.jpg" }
 		]
 	});
+	$("#link-wit").vegas({
+		overlay: '/media/lib/vegas/overlays/01.png',
+		timer: false,
+		shuffle: true,
+		trainsition: 'fade',
+		slides: [
+			{ src: dir + "ohmachi_1.jpg" },
+			{ src: dir + "nanakura_2.jpg" },
+			{ src: dir + "sonohara_1.jpg" },
+			{ src: dir + "hiraide_4.jpg" }
+		]
+	});
+	$("#link-kalm").vegas({
+		overlay: '/media/lib/vegas/overlays/01.png',
+		timer: false,
+		shuffle: true,
+		trainsition: 'fade',
+		slides: [
+			{ src: dir + "tsuburada_2.jpg" },
+			{ src: dir + "shinsui_2.jpg" },
+			{ src: dir + "tamayodo_2.jpg" },
+			{ src: dir + "mozawa_1.jpg" }
+		]
+	});
+	$("#link-canyon").vegas({
+		overlay: '/media/lib/vegas/overlays/01.png',
+		timer: false,
+		shuffle: true,
+		trainsition: 'fade',
+		slides: [
+			{ src: dir + "shimokubo_1.jpg" },
+			{ src: dir + "shimokubo_2.jpg" },
+			{ src: dir + "takatsudo_3.jpg" },
+			{ src: dir + "takatsudo_1.jpg" }
+		]
+	});
+	$("#link-flow").vegas({
+		overlay: '/media/lib/vegas/overlays/01.png',
+		timer: false,
+		shuffle: true,
+		trainsition: 'fade',
+		slides: [
+			{ src: dir + "naramata_3.jpg" },
+			{ src: dir + "naramata_4.jpg" },
+			{ src: dir + "yagisawa_2.jpg" },
+			{ src: dir + "sudagai_2.jpg" }
+		]
+	});
 });
 </script>
